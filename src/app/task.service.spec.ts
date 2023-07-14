5ef8c7648c8da270abb4f114d7749df47d33a7d3
janaGit <janaGit@users.noreply.github.com>
2023-07-14 23:45:46 +0200

    Implement TaskService
    - Write methods to return, add, update and delete tasks
    - Write test for the TaskService
    - Install package and change karma configuration
      to run the tests in Firefox

--- src/app/task.service.spec.ts
+++ src/app/task.service.spec.ts
@@ -1,4 +1,5 @@
 import { TestBed } from '@angular/core/testing';
+import { Task } from './model/task';
 
 import { TaskService } from './task.service';
 
@@ -12,5 +13,92 @@ describe('TaskService', () => {
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should add a task', done => {
+    expect(service.tasks.length).toBe(0);
+
+    service.tasks$.subscribe(tasks=>{
+      expect(tasks.length).toBe(1);
+      expect(tasks.at(0)?.title).toBe('First task');
+      expect(tasks.at(0)?.isCompleted).toBe(false);
+
+      done();
+    });
+
+    service.addTask({title:'First task', isCompleted:false});
+  });
+
+  it('should return all task', () => {
+    expect(service.tasks.length).toBe(0);
+
+    service.tasks.push({title:'First task', isCompleted:false});
+    service.tasks.push({title:'Second task', isCompleted:false});
+    service.tasks.push({title:'Third task', isCompleted:false});
+
+    expect(service.getTasks().length).toBe(3);
+    expect(service.getTasks().at(0)?.title).toBe('First task');
+    expect(service.getTasks().at(1)?.title).toBe('Second task');
+    expect(service.getTasks().at(2)?.title).toBe('Third task');
+  });
+
+  it('should add a task', done => {
+    expect(service.tasks.length).toBe(0);
+
+    service.tasks$.subscribe(tasks=>{
+      expect(tasks.length).toBe(3);
+      expect(tasks.at(0)?.title).toBe('First task');
+      expect(tasks.at(1)?.title).toBe('Second task');
+      expect(tasks.at(2)?.title).toBe('Third task');
+
+      done();
+    });
+
+    service.tasks.push({title:'First task', isCompleted:false});
+    service.tasks.push({title:'Second task', isCompleted:false});
+    service.addTask({title:'Third task', isCompleted:false});
+  });
+
+  it('should update a task', done => {
+    const task: Task = {title:'First task', isCompleted:false};
+
+    expect(service.tasks.length).toBe(0);
+    service.tasks.push(task);
+    expect(service.tasks.length).toBe(1);
+
+    service.tasks$.subscribe(tasks=>{
+      expect(tasks.at(0)).toEqual({title:'Updated task', isCompleted:true});
+
+      done();
+    });
+
+    service.updateTask(0, {title:'Updated task', isCompleted:true});
+
+  });
+
+  it('should throw error on missing task list item', () => {
+    expect(service.tasks.length).toBe(0);
+
+    expect(() => service.updateTask(0, {title:'Updated task', isCompleted:true})).toThrow(Error('Task not found!'));
+
+  });
+
+  it('should delete a task', done => {
+    const task: Task = {title:'First task', isCompleted:false};
+    const task2: Task = {title:'Second task', isCompleted:false};
+
+    expect(service.tasks.length).toBe(0);
+    service.tasks.push(task);
+    service.tasks.push(task2);
+    expect(service.tasks.length).toBe(2);
+
+    service.tasks$.subscribe(tasks=>{
+      expect(tasks.length).toBe(1);
+      expect(tasks.at(0)?.title).toBe('Second task');
+
+      done();
+    });
+
+    service.deleteTask(0);
   });
 });
--- src/app/task.service.ts
+++ src/app/task.service.ts
@@ -1,9 +1,42 @@
 import { Injectable } from '@angular/core';
+import { Task } from './model/task';
+import { Subject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
+  tasks: Task[] = [];
+  tasksSource = new Subject <Task[]>;
+  tasks$ = this.tasksSource.asObservable();
+
   constructor() { }
+
+  getTasks() {
+    return this.tasks;
+  }
+
+  addTask(task: Task) {
+    this.tasks.push(task);
+    this.tasksSource.next(this.tasks);
+  }
+
+  updateTask(index:number,updatedTask: Task) {
+    let task = this.tasks.at(index);
+
+    if(task === undefined) {
+      throw new Error('Task not found!');
+    }else{
+      task.title = updatedTask.title;
+      task.isCompleted = updatedTask.isCompleted;
+    }
+    this.tasksSource.next(this.tasks);
+  }
+
+  deleteTask(index: number) {
+    this.tasks.splice(index, 1);
+    this.tasksSource.next(this.tasks);
+  }
+
 }
